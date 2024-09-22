61f638bbaf0ea0b1c21e316c84295f9227c65182
shumyla <159416618+shumaila34@users.noreply.github.com>
2024-09-23 01:20:36 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Add paths to your files here
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
